47366607ef187e36c82cd2affccec3a8c41b143c
colazeta <47261299+colazeta@users.noreply.github.com>
2026-09-06 21:06:33 +0200

    Apply parsed-record operational schema patch

--- db/apply.sql
+++ db/apply.sql
@@ -7,6 +7,7 @@ BEGIN;
 \ir schema/030_whitelist.sql
 \ir schema/040_source.sql
 \ir schema/045_capture_operational_metadata.sql
+\ir schema/046_parse_operational_metadata.sql
 \ir schema/050_mapping_provenance.sql
 \ir schema/060_derived_governance.sql
 \ir schema/070_views.sql
