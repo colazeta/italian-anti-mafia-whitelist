623c52bd60b373ff9d7ba2be2be64008ed98b893
colazeta <47261299+colazeta@users.noreply.github.com>
2026-09-06 23:11:26 +0200

    Apply versioned semantic resolution migration

--- db/apply.sql
+++ db/apply.sql
@@ -10,6 +10,7 @@ BEGIN;
 \ir schema/046_parse_operational_metadata.sql
 \ir schema/050_mapping_provenance.sql
 \ir schema/055_semantic_projection.sql
+\ir schema/056_semantic_resolution_versioning.sql
 \ir schema/060_derived_governance.sql
 \ir schema/070_views.sql
 \ir seeds/010_lookup_values.sql
