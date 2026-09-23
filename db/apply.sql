ccab273d5f875bc5275f657d5436d0dece7c720c
colazeta <47261299+colazeta@users.noreply.github.com>
2026-09-23 08:10:29 +0200

    Apply multi-input parse lineage schema

--- db/apply.sql
+++ db/apply.sql
@@ -12,6 +12,7 @@ BEGIN;
 \ir schema/047_temporal_capture_identity.sql
 \ir schema/048_capture_series_link.sql
 \ir schema/049_parse_snapshots.sql
+\ir schema/049_parse_snapshot_inputs.sql
 \ir schema/050_mapping_provenance.sql
 \ir schema/055_semantic_projection.sql
 \ir schema/056_semantic_resolution_versioning.sql
